8a0e4e38f865a035edbdeb3fc43330ecb2208c63
LLT <instrafi@Instrafis-Mac-mini.local>
2026-04-23 08:59:46 +0200

    filtered out unnecessary columns for staging layer

--- airstats/models/src/src_airports.sql
+++ airstats/models/src/src_airports.sql
@@ -2,23 +2,14 @@ WITH src_airports AS (
     SELECT * FROM AIRSTATS.RAW.AIRPORTS
 )
 
-SELECT ID AS airport_id,
+SELECT 
        IDENT AS airport_ident,
        TYPE AS airport_type,
        NAME AS airport_name,
-       LATITUDE_DEG,
-       LONGITUDE_DEG, 
-       ELEVATION_FT, 
-       CONTINENT, 
-       ISO_COUNTRY, 
-       ISO_REGION, 
-       MUNICIPALITY, 
-       SCHEDULED_SERVICE, 
-       GPS_CODE, 
-       IATA_CODE, 
-       LOCAL_CODE, 
-       HOME_LINK, 
-       WIKIPEDIA_LINK, 
-       KEYWORDS
+       LATITUDE_DEG AS airport_lat,
+       LONGITUDE_DEG AS airport_long,
+       CONTINENT,
+       ISO_COUNTRY,
+       ISO_REGION
 FROM 
 src_airports
